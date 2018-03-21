03eda337b50df8660387f1c7e3f1c073579b0abd
shalabh-a <37323672+shalabh-a@users.noreply.github.com>
2018-03-21 13:04:42 +0530

    Update ec2v3.tf

--- ec2v3.tf
+++ ec2v3.tf
@@ -3,12 +3,12 @@ provider "aws" {
   alias = "instance3"
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 #Configure the AWS EC2resource
 resource "aws_instance" "instance3" {
-  ami = "ami-0b1e356e"
+  ami = "ami-26ebbc5c"
   instance_type = "t2.micro"
   #availability_zone = "us-east-1a"
   #key_name = "key1"
