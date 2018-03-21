63f77efb1fcd1a94431a68cd78dcdf86374bcaf4
shalabh-a <37323672+shalabh-a@users.noreply.github.com>
2018-03-21 13:00:23 +0530

    Create ec2v3.tf

---
+++ ec2v3.tf
@@ -0,0 +1,22 @@
+#Configure the AWS provider
+provider "aws" {
+  alias = "instance3"
+  region = "us-east-2"
+}
+
+#Configure the AWS EC2resource
+resource "aws_instance" "instance3" {
+  ami = "ami-0b1e356e"
+  instance_type = "t2.micro"
+  #availability_zone = "us-east-1a"
+  #key_name = "key1"
+  #security_groups = ["sg-d4b425a0"]
+  #subnet_id = "subnet-2b059476" 
+  tags {
+    Name = "hostTerraform3"
+  }
+  
+  lifecycle {
+    ignore_changes = ["security_group"]
+  }
+}
