5a1026cfe07241eaf7044fcd5a0cc6ae1ba610a3
shalabh-a <37323672+shalabh-a@users.noreply.github.com>
2018-03-21 12:12:36 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,9 @@
+#Declaring variable
+variable "access_key" {}
+variable "secret_key" {}
+
+#Configure the AWS provider
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
